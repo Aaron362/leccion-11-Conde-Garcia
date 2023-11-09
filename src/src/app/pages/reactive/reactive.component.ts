2bb86e0f99ab4e648491a2f132dbef6ab78f68a0
Aaron <daaroncg135@gmail.com>
2023-11-09 12:38:35 -0600

    Decimo Noveno

--- src/src/app/pages/reactive/reactive.component.ts
+++ src/src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -8,16 +8,41 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactiveComponent implements OnInit {
 
-  
-  forma: FormGroup;
+
+  forma!: FormGroup;
 
   constructor( private fb: FormBuilder){
 
     this.crearFormulario();
+    this.cargarDataAlFormulario();
 
   }
 
   ngOnInit(): void {
+  }
+
+  get pasatiempos() {
+    return this.forma.get('pasatiempos') as FormArray;
+  }
+
+  get nombreNoValido() {
+    return this.forma.get('nombre')?.invalid && this.forma.get('nombre')?.touched
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido')?.invalid && this.forma.get('apellido')?.touched
+  }
+
+  get correoNoValido() {
+    return this.forma.get('correo')?.invalid && this.forma.get('correo')?.touched
+  }
+
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito')?.invalid && this.forma.get('direccion.distrito')?.touched
+  }
+
+  get ciudadNoValido() {
+    return this.forma.get('direccion.ciudad')?.invalid && this.forma.get('direccion.ciudad')?.touched
   }
 
   crearFormulario() {
@@ -26,15 +51,54 @@ export class ReactiveComponent implements OnInit {
       nombre  : ['', [ Validators.minLength(5), Validators.required  ] ],
       apellido: ['', Validators.required ],
       correo  : ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required],
+        ciudad  : ['', Validators.required],
+      }),
+      pasatiempos: this.fb.array([])
 
     });
-  
+
   }
 
 
+  cargarDataAlFormulario() {
 
-  guardar(){ 
+    this.forma.reset({
+      nombre: 'Fernando',
+      apellido: 'Perez',
+      correo: 'juam@gmail.com',
+      direccion: {
+        distrito: 'Ontario',
+        ciudad: 'Ottawa'
+      }
+    });
+
+  }
+
+  agregarPasatiempo(){
+    this.pasatiempos.push( this.fb.control('') );
+  }
+
+  borrarPasatiempos(i : number){
+    this.pasatiempos.removeAt(i);
+  }
+
+  guardar(){
     console.log( this.forma );
+
+    if( this.forma.invalid){
+      Object.values( this.forma.controls).forEach(control =>{
+        control.markAllAsTouched();
+        console.log( control );
+      });
+    }
+
+
+    // Posteo de informacion
+    this.forma.reset({
+      nombre: 'sin nombre'
+    });
   }
- 
+
 }
