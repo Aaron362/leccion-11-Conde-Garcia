9d1a8c2b0e6cd91305b8017a92a1533b2210a1d7
Aaron <daaroncg135@gmail.com>
2023-11-09 16:54:59 -0600

    21 video

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidadoresService } from '../../services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -9,9 +10,11 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ReactiveComponent implements OnInit {
 
 
-  forma!: FormGroup;
+  forma: FormGroup;
 
-  constructor( private fb: FormBuilder){
+
+  constructor( private fb: FormBuilder,
+               private validadores: ValidadoresService){
 
     this.crearFormulario();
     this.cargarDataAlFormulario();
@@ -21,8 +24,8 @@ export class ReactiveComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  get pasatiempos() {
-    return this.forma.get('pasatiempos') as FormArray;
+  get pasatiempos (){
+    return this.forma.get('pasatiempos') as FormArray ;
   }
 
   get nombreNoValido() {
@@ -45,20 +48,37 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get('direccion.ciudad')?.invalid && this.forma.get('direccion.ciudad')?.touched
   }
 
+  get pass1NoValido() {
+    return this.forma.get('pass1')?.invalid && this.forma.get('pass1')?.touched
+  }
+
+  get pass2NoValido() {
+    const pass1 = this.forma.get('pass1')?.value ;
+    const pass2 = this.forma.get('pass2')?.value;
+
+    return ( pass1 === pass2 ) ? false : true;
+
+  }
+
+
+
+
   crearFormulario() {
 
     this.forma = this.fb.group({
       nombre  : ['', [ Validators.minLength(5), Validators.required  ] ],
       apellido: ['', Validators.required ],
       correo  : ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
+      pass1   : ['',  Validators.required],
+      pass2   : ['',  Validators.required],
       direccion: this.fb.group({
         distrito: ['', Validators.required],
         ciudad  : ['', Validators.required],
       }),
       pasatiempos: this.fb.array([
-        [],[],[],[],[]
       ])
-
+    },{
+      Validators: this.validadores.passwordsIguales( 'pass1', 'pass2')
     });
 
   }
@@ -69,7 +89,7 @@ export class ReactiveComponent implements OnInit {
     this.forma.reset({
       nombre: 'Fernando',
       apellido: 'Perez',
-      correo: 'fernando@gmail.com',
+      correo: 'juam@gmail.com',
       direccion: {
         distrito: 'Ontario',
         ciudad: 'Ottawa'
@@ -77,6 +97,15 @@ export class ReactiveComponent implements OnInit {
     });
 
   }
+
+  agregarPasatiempo(){
+    this.pasatiempos.push( this.fb.control('Nuevo elemento', Validators.required));
+  }
+
+  borrarPasatiempos(i : number){
+    this.pasatiempos.removeAt(i);
+  }
+
 
 
 
